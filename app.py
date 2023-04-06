2c80c771bff6e13d0fea3ef6077c7d9ca9487964
Ii tae jeong <52625664+tteon@users.noreply.github.com>
2023-04-06 13:02:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,12 +82,12 @@ openai.api_key = st.secrets["openai_api_key"]
 
 # web
 col1, _, col3 = st.columns([2,0.5,8])
-	with col1:
-	  st.header('Pros')
-	  st.title('기업어때')
+with col1:
+	st.header('Pros')
+	st.title('기업어때')
 
-	with col2:
-	  st.markdown(img_to_html(img_path='img/기업어때_Icon500x500_favicon_1.png', width= 100, height = 100), unsafe_allow_html=True)
+with col2:
+	st.markdown(img_to_html(img_path='img/기업어때_Icon500x500_favicon_1.png', width= 100, height = 100), unsafe_allow_html=True)
 	
 st.title('기업어때')
 st.markdown(body = img_to_html(img_path='img/기업어때_Icon500x500_favicon_2.png' , width= 100, height = 100), unsafe_allow_html=True)
