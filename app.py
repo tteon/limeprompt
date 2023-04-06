f760e90b7a9ce7cd5acfa8227cc77df3c390ae3d
Ii tae jeong <52625664+tteon@users.noreply.github.com>
2023-04-06 22:11:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -293,9 +293,7 @@ try:
                 # report shoot
                 if response:
                     waitparagraph.empty()
-                    # report = paragraph_preprocessing(response['choices'][0]['message']['content'])
-                    # st.text_area(label = f'{advisor} 의 레포팅입니다', value = report, height=col2height)
-                    report_none = response['choices'][0]['message']['content']
+                    report = response['choices'][0]['message']['content']
                     st.text_area(label = f'{advisor} 의 레포팅입니다', value = report_none, height=col2height)
 
     st.download_button(label='download reports', data=report, file_name=f'{advisor} with {companylongname}.txt', mime='text/plain')
